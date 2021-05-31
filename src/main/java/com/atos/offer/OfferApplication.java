ee46e26ab57672d9157e3ef968e726e0ad4b1fb2
AnaisVacquier <anais.vacquier.pro@gmail.com>
2021-05-31 11:32:34 +0200

    add main class

---
+++ src/main/java/com/atos/offer/OfferApplication.java
@@ -0,0 +1,19 @@
+package com.atos.offer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.atos.offer.database.MongoDBConnection;
+import com.mongodb.MongoClient;
+
+@SpringBootApplication
+public class OfferApplication {
+
+  public static void main(String[] args) {
+      SpringApplication.run(OfferApplication.class, args);
+      
+      //connection to the Mongo Database
+      MongoDBConnection mongoDBConnection = MongoDBConnection.getInstance();
+      MongoClient mongoClient = mongoDBConnection.getMongoClient();
+  }
+}
